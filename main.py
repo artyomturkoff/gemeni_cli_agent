bc42cc5e9556662868d8202987bac4f9f668488f
artyomturkoff <art.turkoff@gmail.com>
2026-03-08 17:53:24 +0100

    Promt and response token usage added from response metadata

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
-        raise RuntimeError('No GEMINI_API_KEY found')
+        raise RuntimeError("No GEMINI_API_KEY found")
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
         )
+    if not response.usage_metadata:
+        raise RuntimeError("No response metadata")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:")
     print(f"{response.text}")
 

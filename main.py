70018e014f25782ecd7cc98389cf20557879ca97
artyomturkoff <art.turkoff@gmail.com>
2026-03-09 18:36:56 +0100

    Optional --verbose argument added to argparse and it's handling;

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 
 
@@ -15,18 +15,26 @@ def main():
 
     parser = argparse.ArgumentParser(description="Gemini Bot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=args.user_prompt
+        contents=messages
         )
     if not response.usage_metadata:
         raise RuntimeError("No response metadata")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if args.verbose:    
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
     print(f"Response:")
     print(f"{response.text}")
+
+
 
 if __name__ == "__main__":
     main()
